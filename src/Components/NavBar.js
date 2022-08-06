73fc0df95f5d0e52b0eea5db06689321764018b3
Harman <herman.muhemed@gmail.com>
2022-08-07 01:42:06 +0300

    hotfix:menu icon color changed,NavLink color chnaged

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -33,7 +33,7 @@ export default function NavBar(props) {
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md  focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white bg-lightmauve hover:bg-mauve hover:text-black">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md  focus:outline-none focus:ring-2 focus:ring-inset focus:ring-black bg-white text-black hover:bg-lightmauve">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -66,7 +66,7 @@ export default function NavBar(props) {
                         className={classNames(
                           location.pathname === item.href
                             ? 'bg-white'
-                            : ' text-white hover:bg-white hover:text-black',
+                            : ' text-white hover:bg-lightmauve hover:text-black',
                           'px-1 py-2 rounded-md text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined} 
@@ -102,7 +102,7 @@ export default function NavBar(props) {
                   className={classNames(
                     location.pathname === item.href
                       ? 'bg-white'
-                      : 'text-white hover:bg-white hover:text-black',
+                      : 'text-white hover:bg-lightmauve hover:text-black',
                     'block px-1 py-2 rounded-md text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
