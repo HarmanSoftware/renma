0507d87c702e097eeaa4742ad4eb803823f8fcbf
Harman <herman.muhemed@gmail.com>
2022-08-11 19:29:35 +0300

    style:Toltip added to Social links, Dark mode added to 404 page

--- src/Pages/PageNotFound.js
+++ src/Pages/PageNotFound.js
@@ -5,12 +5,12 @@ export default function PageNotFound() {
   return (
 	<div>
 
-    <section className="flex bg-white items-center dark:text-black py-4">
+    <section className="flex bg-white items-center dark:text-black py-4 " id='not-found-bg'>
 	<div className="container flex flex-col items-center justify-center mx-auto text-center sm:max-w-md">
 		<img src={NotFound} alt="Page not found" />
 		<div className="text-3xl py-8">Ooops, The page you looking for can't be found!</div>
 		<Link to="/">
-			 <button className=" flex items-center justify-center mx-auto text-center font-medium rounded-sm bg-mauve hover:bg-lightmauve p-2">
+			 <button className=" flex items-center justify-center mx-auto text-center font-medium rounded-sm bg-mauve hover:bg-lightpurple p-2" id='back-home-btn-not-found'>
 			   Back Home
 		     </button>
 		</Link>
