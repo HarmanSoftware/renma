9fba3e9efcff89e5cf6201199fbd986e44a31786
Harman <herman.muhemed@gmail.com>
2022-08-14 22:41:03 +0300

    Page: About us page added

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,12 +4,13 @@ export default function About() {
   return (
     <div>
       <div className='flex items-center content-center bg-mauve font-medium text-3xl px-2 w-ful h-12 text-black'>About Us</div>
-      <img src={AboutUs} className='w-3/5 h-1/5 mx-auto' alt="about us img"/>
+      <img src={AboutUs} className='w-2/4 h-1/5 mx-auto' alt="about us img"/>
       <div className='container text-center mx-auto'>
-      <p className=' px-4 py-1 font-normal text-lg text-justify'>       
-       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet lacus pulvinar, rhoncus massa a, egestas eros. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque pretium gravida enim at efficitur. In finibus felis non magna tincidunt, eget placerat libero dignissim. Donec lacinia eros vel est sodales, et fringilla justo porta. Cras a pretium nibh. Vestibulum mattis sollicitudin erat, quis posuere erat viverra a. In rhoncus eleifend sapien eget mattis. Morbi id sapien ante. Fusce ut lectus ut tortor porta facilisis. Phasellus vulputate, nisl at egestas feugiat, orci turpis tincidunt magna, eu auctor dui arcu sed elit. In vel iaculis tortor, eget vehicula libero. Fusce eget sodales orci. Sed ullamcorper vitae dui eget molestie. Quisque rutrum in felis vitae dapibus.
-       Aliquam laoreet imperdiet interdum. Proin efficitur neque sit amet porttitor ultricies. Sed tincidunt urna ut massa elementum sagittis. Nam viverra volutpat mattis. Maecenas interdum tellus eu lorem egestas, vitae vestibulum mi imperdiet. Nulla facilisi. Sed nec est ac ante consequat eleifend. Nulla consectetur mauris non metus maximus, et sollicitudin lorem porttitor. Donec sed lorem ipsum. Praesent non mi eget ipsum rhoncus tincidunt. Phasellus congue ultrices ultrices. Suspendisse in pulvinar leo. Nulla feugiat vestibulum ipsum non eleifend. Duis posuere imperdiet nunc ac luctus.
-       </p>      
+      <p className='px-4 mb-3 font-normal text-lg text-justify'>   
+      Our team of physiotherapists boast over 100 years of combined experienced. We provide an honest and ethical physiotherapy service to the residents of London and beyond.
+      <br/><br/>
+      Our clinic is based in the centre of London and is very easy to access for everyone. We offer a whole range of physiotherapy services no matter what injury, strain or pain you may have.    
+      </p>      
       </div>
     </div>
   )
