528258cecb618f4de85b0b94acf65a6de76bcfa9
Harman <herman.muhemed@gmail.com>
2022-08-14 22:19:38 +0300

    Feature:Validation added to contact us form

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,7 +1,16 @@
 import React from 'react'
-
+import AboutUs from '../Assets/Images/AboutUs.png'
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+      <div className='flex items-center content-center bg-mauve font-medium text-3xl px-2 w-ful h-12 text-black'>About Us</div>
+      <img src={AboutUs} className='w-3/5 h-1/5 mx-auto' alt="about us img"/>
+      <div className='container text-center mx-auto'>
+      <p className=' px-4 py-1 font-normal text-lg text-justify'>       
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet lacus pulvinar, rhoncus massa a, egestas eros. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque pretium gravida enim at efficitur. In finibus felis non magna tincidunt, eget placerat libero dignissim. Donec lacinia eros vel est sodales, et fringilla justo porta. Cras a pretium nibh. Vestibulum mattis sollicitudin erat, quis posuere erat viverra a. In rhoncus eleifend sapien eget mattis. Morbi id sapien ante. Fusce ut lectus ut tortor porta facilisis. Phasellus vulputate, nisl at egestas feugiat, orci turpis tincidunt magna, eu auctor dui arcu sed elit. In vel iaculis tortor, eget vehicula libero. Fusce eget sodales orci. Sed ullamcorper vitae dui eget molestie. Quisque rutrum in felis vitae dapibus.
+       Aliquam laoreet imperdiet interdum. Proin efficitur neque sit amet porttitor ultricies. Sed tincidunt urna ut massa elementum sagittis. Nam viverra volutpat mattis. Maecenas interdum tellus eu lorem egestas, vitae vestibulum mi imperdiet. Nulla facilisi. Sed nec est ac ante consequat eleifend. Nulla consectetur mauris non metus maximus, et sollicitudin lorem porttitor. Donec sed lorem ipsum. Praesent non mi eget ipsum rhoncus tincidunt. Phasellus congue ultrices ultrices. Suspendisse in pulvinar leo. Nulla feugiat vestibulum ipsum non eleifend. Duis posuere imperdiet nunc ac luctus.
+       </p>      
+      </div>
+    </div>
   )
 }
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -2,36 +2,81 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import Tippy from '@tippyjs/react';
 import '../Assets/DarkMode.css';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 import Email from '../Assets/Images/Emails.png'
 import Call from '../Assets/Images/Call.png'
-import Chat from '../Assets/Images/Chatting.png'
 import ContactUs from '../Assets/Images/Contact.png'
 import Mail from '../Assets/Images/Send.png'
 
 export default function Contact() {
+  const formik = useFormik({
+    initialValues: {
+      fullName: '',
+      email: '',
+      phoneNumber: '',
+      message: '',
+    },
+    validationSchema: Yup.object({
+      fullName: Yup.string()
+       .required('Full name required')
+       .max(30, 'Fullname must be 30 characters or less')
+       .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
+      email: Yup.string()
+       .email('Invalid email address')
+       .required('Required'),
+       phoneNumber: Yup.number()
+       .typeError("That doesn't look like a phone number")
+       .positive("A phone number can't start with a minus")
+       .integer("A phone number can't include a decimal point")
+       .min(8)
+       .required('A phone number is required'),
+       message:Yup.string()
+       .required('Messege is required')
+       .max(10,'Messege must be 150 or less chars')
+    }),
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <div>
+      <div className='flex items-center content-center bg-mauve font-medium text-3xl px-2 w-ful h-12 text-black'>Contact Us</div>
       <div className='container mx-auto grid lg:grid-cols-2 sm:w-auto sm:grid-cols-1 p-8'>
         <div className='bg-soft-purple my-4' id="contact-us">
 <form className='p-4'>  
             <div className='text-center font-medium text-4xl text-white py-3'>Contact Us</div>
             <div className='text-center font-medium text-2xl text-white py-3'> <span className='text-red-500'>*</span> Indicates Required Fields</div>
 <div class="relative z-0 mb-6 w-full group py-1">
-      <input type="text" name="name" id="name" className="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-mauve dark:focus:border-lightpurple focus:outline-none focus:ring-0 focus:border-white peer" placeholder=" " required=""/>
+      <input type="text" name="name" id="name"
+       {...formik.getFieldProps('fullName')}
+      className="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-mauve dark:focus:border-lightpurple focus:outline-none focus:ring-0 focus:border-white peer" placeholder=" " required=""/>
       <label for="name" className="peer-focus:font-medium absolute text-xl text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"><span className='text-red-500 font-medium text-xl'>*</span> Full name</label>
+      {formik.touched.fullName && formik.errors.fullName ?
+      (<div className='text-white py-2 text-center'>{formik.errors.fullName}</div> ) : null}
+ 
   </div>
   <div class="relative z-0 mb-6 w-full group py-1">
-      <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-mauve dark:focus:border-lightpurple focus:outline-none focus:ring-0 focus:border-white peer" placeholder=" " required=""/>
+      <input type="email" name="floating_email" id="floating_email"
+         {...formik.getFieldProps('email')} className="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-mauve dark:focus:border-lightpurple focus:outline-none focus:ring-0 focus:border-white peer" placeholder=" " required=""/>
       <label for="floating_email" className="peer-focus:font-medium absolute text-lg text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"><span className='text-red-500 font-medium text-xl'>*</span> Email address</label>
+      {formik.touched.email && formik.errors.email ?
+      (<div className='text-white py-2 text-center'>{formik.errors.email}</div> ) : null}
   </div>
   <div class="relative z-0 mb-6 w-full group py-1">
-      <input type="tel" name="phone_number" id="phone_number" className="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-mauve dark:focus:border-lightpurple focus:outline-none focus:ring-0 focus:border-white peer" placeholder=" "/>
+      <input type="tel" name="phone_number" id="phone_number"
+      {...formik.getFieldProps('phoneNumber')} className="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-mauve dark:focus:border-lightpurple focus:outline-none focus:ring-0 focus:border-white peer" placeholder=" "/>
       <label for="phone_number" className="peer-focus:font-medium absolute text-lg text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (Optional)</label>
+      {formik.touched.phoneNumber && formik.errors.phoneNumber ?
+      (<div className='text-white py-2 text-center'>{formik.errors.phoneNumber}</div> ) : null}
   </div>  
   <div class="relative z-0 mb-6 w-full group py-1">
-      <textarea type="text" name="text-area" id="text-area" className="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-mauve dark:focus:border-lightpurple focus:outline-none focus:ring-0 focus:border-white peer rows=2 cols=50" placeholder=" " required=""/>
+      <textarea type="text" name="text-area" id="text-area"
+      {...formik.getFieldProps('message')} className="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-mauve dark:focus:border-lightpurple focus:outline-none focus:ring-0 focus:border-white peer rows=2 cols=50" placeholder=" " required=""/>
       <label for="text_area" className="peer-focus:font-medium absolute text-lg text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"><span className='text-red-500 font-medium text-xl'>*</span> Message</label>
+      {formik.touched.message && formik.errors.message ?
+      (<div className='text-white py-2 text-center'>{formik.errors.message}</div> ) : null}
   </div>  
   
   <button type="submit" id="send-btn" className="flex items-center content-center mx-auto bg-bmauve hover:bg-lightmauve focus:ring-4 focus:outline-none focus:ring-mauve font-medium rounded-lg text-lg lg:w-26 lg:h-14 md:w-32 md:h-12 sm:w-18 sm:h-12 px-4 py-2 text-center dark:bg-mauve dark:hover:bg-lightpurple dark:focus:ring-mauve">
